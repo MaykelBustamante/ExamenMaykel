077bd078dfa8d5051e56bfc231db4b00c367f622
MAYKEL DAVID BUSTAMANTE <maykel.bustamante.c@gmail.com>
2022-03-02 18:40:42 -0600

    Agregar archivos de proyecto.

---
+++ BaseDeDatos/dbo/StoreProcedures/ParametroInsertar.sql
@@ -0,0 +1,44 @@
+﻿CREATE PROCEDURE [dbo].[ParametroInsertar]
+	@Id_Parametro int,
+	@Codigo varchar(250),
+	@Descripcion  varchar(250),
+	@Valor  varchar(250),
+	@Estado bit
+AS BEGIN
+SET NOCOUNT ON
+
+  BEGIN TRANSACTION TRASA 
+    
+	BEGIN TRY
+
+		 INSERT INTO dbo.Parametro
+		 ( Id_Parametro,
+		   Codigo,
+		   Descripcion,
+		   Valor,
+		   Estado
+		 )
+		 VALUES
+		 ( @Id_Parametro,
+		   @Codigo,
+		   @Descripcion,
+		   @Valor,
+		   @Estado	 
+		 )
+
+	 COMMIT TRANSACTION TRASA
+	 SELECT 0 AS CodeError, '' AS MsgError
+
+	END TRY
+
+	BEGIN CATCH
+
+	 SELECT 
+	       ERROR_NUMBER() AS CodeError,
+		   ERROR_MESSAGE() AS MsgError
+
+     ROLLBACK TRANSACTION TRASA
+
+	END CATCH
+
+  END
---
+++ WBL/ParametroService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBL
+{
+     public interface IProveedorService
+    {
+        Task<DBEntity> Create(ParametroEntity entity);
+        Task<DBEntity> Delete(ParametroEntity entity);
+        Task<IEnumerable<ParametroEntity>> Get();
+        Task<ParametroEntity> GetById(ParametroEntity entity);
+        Task<DBEntity> Update(ParametroEntity entity);
+    }
+    class ParametroService
+    {
+    }
+}
